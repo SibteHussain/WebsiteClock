073aa3941cf24411101482c6f20706dc9e970f2b
SibteHussain <sibte566@gmail.com>
2022-08-23 10:44:27 -0700

    first commit

---
+++ backend.js
@@ -0,0 +1,13 @@
+function showtime(){
+    const d=new Date();
+year=d.getFullYear();
+date=d.getDate();
+day=d.getDay();
+month=d.toLocaleString('default', { month: 'long' });
+hour=d.getHours();
+minutes=d.getMinutes();
+seconds=d.getSeconds();
+document.getElementById("date").innerHTML= `Today\'s date is is ${date}` + " " + month + " " + year;
+document.getElementById("time").innerHTML=hour + ': ' + minutes + ': ' +seconds;
+console.log(hours);
+}setInterval(showtime, 1000);
